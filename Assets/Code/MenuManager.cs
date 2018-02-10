a1aa7e33ef4d82b59ca63f005e8fde135c2cc61e
zbearwrestler <z.bearinger@gmail.com>
2018-02-10 10:04:26 -0800

    Main Menu Work + Music Tracks
    Menu system working mostly still alot of work to be done.  Music V1's are in listen to them and give feedback, someone can start messing with having an audio mixer and changing play speed.  Scott said we can go down to 80% and up to 115% or so if needed,  we can also get an even more intese track.

---
+++ Assets/Code/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Test_TalkingHeads", LoadSceneMode.Single);
+    }
+
+    public void TangentialLearning()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
