55b390d41f16c8ff37dafe8215c35dd703b43f68
Krunkery <simonlthorp@gmail.com>
2018-02-11 09:23:35 -0800

    Music Transition w/ Prefab

---
+++ Assets/Code/MusicManagerCreater.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManagerCreater : MonoBehaviour {
+
+    public GameObject musicManager;
+    public Transform m_MainCamera;
+
+	// Use this for initialization
+	void Start () {
+
+        musicManager = (GameObject)Instantiate(musicManager, m_MainCamera);
+
+	}
+	
+}
