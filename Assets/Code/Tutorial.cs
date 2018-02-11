586c01d247e4d923479c69586b6fdca564eba59b
zbearwrestler <z.bearinger@gmail.com>
2018-02-11 14:34:42 -0800

    Update Menus added tutorial
    Added the Tutorial and updated menus again

---
+++ Assets/Code/Tutorial.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour {
+    public float timer = 5f;
+    public Sprite[] comics;
+    public int current = 0;
+    public int max = 5;
+    public GameObject viewer;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine("DisplayScene");
+    }
+
+    IEnumerator DisplayScene()
+    {
+        yield return new WaitForSeconds(timer);
+        current++;
+        if (current == max)
+        {
+            SceneManager.LoadScene("TalkingHeads", LoadSceneMode.Single);
+        }
+        viewer.GetComponent<Image>().sprite = comics[current];
+        StartCoroutine("DisplayScene");
+    }
+}
